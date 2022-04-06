21b297c5e29251d7c966854e41bd856d377ec118
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-04-06 21:32:56 +0200

    stan projektu przed zadaniem 23.4

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -11,35 +11,4 @@ public class Mail {
     private final String subject;
     private final String message;
     private final String toCc;
-
-    /*public static class MailBuilder {
-        private String mailTo;
-        private String subject;
-        private String message;
-        private String toCc;
-
-        public MailBuilder setMailTo(String mailTo) {
-            this.mailTo = mailTo;
-            return this;
-        }
-
-        public MailBuilder setSubject(String subject) {
-            this.subject = subject;
-            return this;
-        }
-
-        public MailBuilder setMessage(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public MailBuilder setToCc(String toCc) {
-            this.toCc = toCc;
-            return this;
-        }
-
-        public Mail build() {
-            return new Mail(mailTo, subject, message, toCc);
-        }
-    }*/
 }
